d06240e5b0a902a29c0889afe37838dafc1137dd
nouf almutairi <39540611+nouf-falmutairi@users.noreply.github.com>
2025-10-05 17:38:58 +0300

    added setup and pyproject

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nasa_study_search",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+	"serverless-wsgi",
+        "httpx",
+        "pandas",
+        "spacy",
+        "gensim"
+    ],
+    entry_points={
+        "console_scripts": [
+            "nasa-app=app:app",
+        ],
+    },
+)
